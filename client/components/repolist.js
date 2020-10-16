17152e82b1c3c4c1af27fc693279fdcf194e40a0
lena <67883156+huanlina@users.noreply.github.com>
2020-10-16 23:49:44 +0800

    repolist update

--- client/components/repolist.js
+++ client/components/repolist.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
 import Header from './header'
